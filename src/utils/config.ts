2f3ef38629484a6eec0815c529aa115854a05526
WEICENGXING <2997657261@qq.com>
2025-06-21 16:55:48 +0800

    ahha

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,2 +1,3 @@
-export const userurl='https://352f-58-194-169-164.ngrok-free.app '
-export const chaturl='https://b0fc-58-194-169-164.ngrok-free.app '
+
+export const userurl='https://b0fc-58-194-169-164.ngrok-free.app'
+export const chaturl='https://352f-58-194-169-164.ngrok-free.app'
